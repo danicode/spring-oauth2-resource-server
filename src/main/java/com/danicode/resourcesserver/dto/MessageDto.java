18a5931b0b81042cdeb770a427c42d88ff59428f
danicode <danielanzawa@gmail.com>
2023-08-04 18:31:23 -0300

    Capitulo 4: arrancando servidor de recursos.

---
+++ src/main/java/com/danicode/resourcesserver/dto/MessageDto.java
@@ -0,0 +1,4 @@
+package com.danicode.resourcesserver.dto;
+
+public record MessageDto(String message) {
+}
---
+++ src/test/java/com/danicode/resourcesserver/ResourcesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danicode.resourcesserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourcesServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
